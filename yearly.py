5d869ff9a0edc1decf7c56562543cd8c3732fd75
Product <Product@TechAdmins-MacBook-Pro.local>
2024-01-22 17:28:42 +0300

    added date

--- yearly.py
+++ yearly.py
@@ -2,5 +2,6 @@ import calendar
 
 yy= int(input("Enter year to display: "))
 mm= int(input("Enter month to display: "))
+dd= int(input("Enter day to display: "))
 
-print(calendar.month(yy,mm))
+print(calendar.month(yy,mm,dd))
