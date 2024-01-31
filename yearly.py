23b5c71461572dd5f6040dbe81f7de539c0e1073
Product <Product@TechAdmins-MacBook-Pro.local>
2024-01-31 17:34:27 +0300

    added capitalize and strip methods

--- yearly.py
+++ yearly.py
@@ -1,7 +1,29 @@
-import calendar
+# import calendar
 
-yy= int(input("Enter year to display: "))
-mm= int(input("Enter month to display: "))
-dd= int(input("Enter day to display: "))
+# yy= int(input("Enter year to display: "))
+# mm= int(input("Enter month to display: "))
+# dd= int(input("Enter day to display: "))
 
-print(calendar.month(yy,mm,dd))
+# print(calendar.month(yy,mm,dd))
+
+# fu
+
+# random=input().capitalize()
+# print(random)
+
+# random="          any word       "
+# text=random.strip()
+
+# print(text)
+# def password():
+#     word=input().strip()
+#     print(word)
+# password()
+
+def password():
+    random="person"
+    text=random.strip("pn")
+    print(text)
+password()
+    
+   
